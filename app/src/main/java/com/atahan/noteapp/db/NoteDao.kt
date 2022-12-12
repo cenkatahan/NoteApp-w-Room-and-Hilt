3572d81346a9f57d43ab04d870fcd951ed5aca02
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-12 15:36:05 +0300

    fix dao method return type

--- app/src/main/java/com/atahan/noteapp/db/NoteDao.kt
+++ app/src/main/java/com/atahan/noteapp/db/NoteDao.kt
@@ -5,17 +5,16 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.atahan.noteapp.model.NoteEntity
 import com.atahan.noteapp.utils.Constants.NOTE_TABLE
-import java.util.ArrayList
 
 @Dao
 interface NoteDao {
 
     @Query("SELECT * FROM $NOTE_TABLE")
-    fun getNotes(): ArrayList<NoteEntity>
+    fun getNotes(): List<NoteEntity>
 
     @Insert
     fun add(note: NoteEntity)
 
     @Query("DELETE FROM $NOTE_TABLE")
-    fun deleteNotes();
+    fun deleteNotes()
 }
