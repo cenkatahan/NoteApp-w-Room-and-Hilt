88ab3f79c7aefd9df2e575951ff39bf4384d2cab
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-13 10:46:15 +0300

    implement note adapter and recyclerview.

--- app/src/main/java/com/atahan/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/atahan/noteapp/adapter/NoteAdapter.kt
@@ -1,4 +1,53 @@
 package com.atahan.noteapp.adapter
 
-class NoteAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.atahan.noteapp.databinding.LayoutItemNoteBinding
+import com.atahan.noteapp.model.NoteEntity
+import javax.inject.Inject
+
+class NoteAdapter @Inject constructor() : RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
+    private lateinit var binding: LayoutItemNoteBinding
+
+    private val differCallback = object : DiffUtil.ItemCallback<NoteEntity>() {
+        override fun areItemsTheSame(oldItem: NoteEntity, newItem: NoteEntity): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: NoteEntity, newItem: NoteEntity): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        binding = LayoutItemNoteBinding.inflate(inflater, parent, false)
+        return NoteHolder()
+    }
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        holder.bindItem(differ.currentList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class NoteHolder : RecyclerView.ViewHolder(binding.root) {
+        fun bindItem(item: NoteEntity) {
+
+
+            binding.apply {
+                tvItemTitle.text = item.title
+                tvItemDesc.text = item.desc
+            }
+        }
+    }
 }
--- app/src/main/java/com/atahan/noteapp/view/NoteListFragment.kt
+++ app/src/main/java/com/atahan/noteapp/view/NoteListFragment.kt
@@ -6,17 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.atahan.noteapp.R
+import com.atahan.noteapp.adapter.NoteAdapter
 import com.atahan.noteapp.databinding.FragmentNoteListBinding
+import com.atahan.noteapp.model.NoteEntity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NoteListFragment : Fragment() {
     private lateinit var binding: FragmentNoteListBinding
 
+    @Inject
+    lateinit var noteAdapter: NoteAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //fab listener
         binding.fabAdd.setOnClickListener {
             it.findNavController().navigate(R.id.action_noteListFragment_to_noteAddFragment)
         }
+
+        //recyclerview & adapter
+
     }
 
     override fun onCreateView(
@@ -25,6 +39,12 @@ class NoteListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        binding.rvNotes.apply {
+            layoutManager = LinearLayoutManager(this@NoteListFragment.context)
+            adapter = noteAdapter
+        }
+
+
         return binding.root
     }
 
