c95c7aead1618d63d9624906cac04510a991efaa
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-13 11:41:00 +0300

    change add fragment.

--- app/src/main/java/com/atahan/noteapp/view/NoteListFragment.kt
+++ app/src/main/java/com/atahan/noteapp/view/NoteListFragment.kt
@@ -33,13 +33,13 @@ class NoteListFragment : Fragment() {
             it.findNavController().navigate(R.id.action_noteListFragment_to_noteAddFragment)
         }
 
-        repository.add(
-            NoteEntity(
-                0,
-                "TITLE",
-                "Description"
-            )
-        )
+//        repository.add(
+//            NoteEntity(
+//                0,
+//                "TITLE",
+//                "Description"
+//            )
+//        )
     }
 
     override fun onCreateView(
