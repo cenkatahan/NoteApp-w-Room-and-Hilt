5d7c75bd04d6f960a981373658501a6938798e36
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-13 12:21:39 +0300

    implement add fragment.

--- app/src/main/java/com/atahan/noteapp/view/NoteAddFragment.kt
+++ app/src/main/java/com/atahan/noteapp/view/NoteAddFragment.kt
@@ -5,29 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.atahan.noteapp.R
+import com.atahan.noteapp.databinding.FragmentNoteAddBinding
+import com.atahan.noteapp.model.NoteEntity
+import com.atahan.noteapp.repository.NoteRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NoteAddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class NoteAddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentNoteAddBinding
+
+    @Inject
+    lateinit var repository: NoteRepository
+
+    @Inject
+    lateinit var note: NoteEntity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onCreateView(
@@ -35,26 +36,24 @@ class NoteAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_add, container, false)
+        binding = FragmentNoteAddBinding.inflate(inflater, container, false)
+
+        binding.btnAdd.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val desc = binding.etDesc.text.toString()
+
+            repository.add(
+                NoteEntity(
+                    0,
+                    title = title,
+                    desc = desc
+                )
+            )
+
+            it.findNavController().navigate(R.id.action_noteAddFragment_to_noteListFragment)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteAddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteAddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
